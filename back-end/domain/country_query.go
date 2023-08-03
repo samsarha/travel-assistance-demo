9a7f62569a9d9aa1cc4feb561bb47b1006d02c22
Samsarha <27887381+samsarha@users.noreply.github.com>
2023-08-04 00:07:07 +0200

    refactored country_query

--- back-end/domain/country_query.go
+++ back-end/domain/country_query.go
@@ -6,8 +6,5 @@ import (
 )
 
 func QueryCountryData(countryCode string, datasource datasource.CountryDataSource) models.Country {
-
-	weather := []models.Weather{{Date: "123454433443", IconUri: "dshdsdssdf", Min: 25, Max: 12, Description: "Rainy"}}
-	country := models.Country{CountryName: "Zimbabwe", Gdp: 1000, ExchangeRate: 54.30, Weather: weather}
-	return country
+	return datasource.FetchCountryData(countryCode)
 }
