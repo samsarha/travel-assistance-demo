98ee346c6e183ab74a36e5b1373d615e14af75ec
Samsarha <27887381+samsarha@users.noreply.github.com>
2023-08-04 07:45:37 +0200

    small refactor

--- back-end/models/weather.go
+++ back-end/models/weather.go
@@ -2,7 +2,7 @@ package models
 
 type Weather struct {
 	Date        string  `json:"countryname"`
-	IconUri     string  `json:"gdp"`
+	IconUri     string  `json:"iconUri"`
 	Min         float64 `json:"min"`
 	Max         float64 `json:"max"`
 	Description string  `json:"description"`
